2c493c90c9d8baf7f57b184ff043577be8a1f0d8
imrancoder10 <imranhossain1148@gmail.com>
2024-09-30 12:12:58 +0600

    templage

---
+++ Module 30/30-3-template.js
@@ -0,0 +1,38 @@
+const first = 'jan';
+const last = 'pakhi';
+const name = first + ' ' + last;
+// console.log(name);
+
+// ======================================
+
+const a = 10;
+const b = 20;
+const result = 'The sum of ' + a + ' and ' + b + ' is ' + (a + b);
+// console.log(result);
+
+
+// ===================================
+
+const math = `The sum of ${a} and ${b} is ${a+b}`;
+
+// console.log(math);
+// =======================================================
+
+const email = 'Hi john \n' +
+    'cena cena\nlage' +
+    '\ntumi mara mari korba naki';
+// console.log(email);
+// ==========================================
+
+const challenge = `john Cena
+cena naki ochena
+mair dimu prochur`;
+
+// console.log(challenge);
+
+// /======================================
+
+const number = [12, 25, 69, 87];
+const student ={name: 'John Ochena', age : 5};
+const setNumberFromArray = `The sum of ${number[1]} and ${student.age}`;
+console.log(setNumberFromArray);
